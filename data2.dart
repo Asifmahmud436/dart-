5328ff7d154c78ce89ff43f9bbfaa21462f342cb
Zettex <safaandsafa4@gmail.com>
2025-07-01 00:10:15 +0600

    learned loop

---
+++ data2.dart
@@ -0,0 +1,19 @@
+main(){
+
+  // list
+  List names = [1,23413,"Jack"];
+  for (var i in names){
+    print(i);
+  }
+  // set
+  Set halogens = {"Flourine","Chlorine"};
+  print(halogens);
+
+  // map
+  var gifts = {
+    'first':'Asif',
+    'second':'Disha'
+  };
+  print(gifts);
+  var gifts2 = Map();
+}
---
+++ loop.dart
@@ -0,0 +1,9 @@
+main(){
+  for(int i = 1;i<=10;i++){
+    print(i);
+  }
+  var numbers = [1,23,54234235,3452];
+  for(var a in numbers){
+    print("Numbers: $a");
+  }
+}
