04d2ce057bd1f94cadc829201dfcbc64a8e2a4fe
Zettex <safaandsafa4@gmail.com>
2025-06-30 22:57:18 +0600

    learned datatype

---
+++ dataType.dart
@@ -0,0 +1,9 @@
+main(){
+  int amount = 100;
+  double a3 = 100.11;
+  bool isTrue = true;
+  dynamic weakVar = 434534;
+  String a4 = "Asif";
+
+  print("Int:$amount Double:$a3 Bool:$isTrue String:$a4 Dynamic:$weakVar");
+}
